30f1161c88f0cfdebe71ec8624295e123d3b0c5e
Aaron <ymouzhang@gmail.com>
2024-11-27 01:03:49 +0800

    feat:imgui opengl3

---
+++ ImGuiAll.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "ImGui/imconfig.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_opengl3.h"
+#include "ImGui/imgui_impl_win32.h"
+#include "ImGui/imgui_internal.h"
+#include "ImGui/imstb_rectpack.h"
+#include "ImGui/imstb_textedit.h"
+#include "ImGui/imstb_truetype.h"
+#include "ImGui/imgui_impl_glfw.h"
+
+#include <d3d11.h>
+
+#pragma comment(lib,"d3d11.lib")
